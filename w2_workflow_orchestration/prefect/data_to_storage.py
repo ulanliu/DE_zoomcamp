f3845172ea0e11102ee89594ad47d9acf03c8c1a
Peter Liu <f97290650@gmail.com>
2023-01-27 17:12:11 +0800

    Add prefect folder

---
+++ w2_workflow_orchestration/prefect/data_to_storage.py
@@ -0,0 +1,57 @@
+from pathlib import Path
+import pandas as pd
+from prefect import flow, task
+from prefect_gcp.cloud_storage import GcsBucket
+
+@task()
+def fetch(dataset_url: str) -> pd.DataFrame:
+    """Read taxi data from web into DataFrame"""
+    
+    df = pd.read_csv(dataset_url)
+
+    return df
+
+@task(log_prints=True)
+def clean(df: pd.DataFrame) -> pd.DataFrame:
+    """Transform the time format"""
+    
+    df["tpep_pickup_datetime"] = pd.to_datetime(df["tpep_pickup_datetime"])
+    df["tpep_dropoff_datetime"] = pd.to_datetime(df["tpep_dropoff_datetime"])
+    print(df.head(2))
+    print(f"columns: {df.dtypes}")
+    print(f"rows: {len(df)}")
+
+    return df
+
+@task(log_prints=True)
+def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
+    """Write DataFrame out locally as parquet files"""
+    
+    if not Path(f"data/{color}").exists():
+        Path(f"data/{color}").mkdir
+    path = Path(f"data/{color}/{dataset_file}.parquet")
+    df.to_parquet(path, compression="gzip")
+
+    return path
+
+@task()
+def write_gcs(path: Path) -> None:
+    gcs_block = GcsBucket.load("zoomcamp")
+    gcs_block.upload_from_path(from_path=path, to_path=path)
+
+@flow()
+def etl_web_to_gcs() -> None:
+    """The main ETL function"""
+    color = 'yellow'
+    year = 2021
+    month = 1
+    dataset_file = f"{color}_tripdata_{year}-{month:02}"
+    dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
+
+    df = fetch(dataset_url)
+    df_clean = clean(df)
+    path = write_local(df_clean, color, dataset_file)
+    write_gcs(path)
+
+if __name__ == "__main__":
+    etl_web_to_gcs()
