0f306403d94d610c946cbf6d390f6de56fa45248
Peter Liu <f97290650@gmail.com>
2023-03-20 23:01:45 +0800

    Update w7 dbt model

--- w7_final_project/dbt/who_disease_data/models/who_disease_model.sql
+++ w7_final_project/dbt/who_disease_data/models/who_disease_model.sql
@@ -1,4 +1,4 @@
-{{ config(materialized='view') }}
+{{ config(materialized='table') }}
 
 {%- set cols = adapter.get_columns_in_relation(source('staging', 'monkeypox')) -%}
 
@@ -11,11 +11,27 @@ WITH monkeypox AS (
 ),
 
 covid_19 AS (
-    SELECT * FROM {{ source('staging', 'covid-19') }}
+    SELECT 
+        location,
+        iso_code,
+        date,
+        total_cases,
+        total_deaths,
+        new_cases,
+        new_deaths,
+        new_cases_smoothed,
+        new_deaths_smoothed,
+        new_cases_per_million,
+        total_cases_per_million,
+        new_cases_smoothed_per_million,
+        new_deaths_per_million,
+        total_deaths_per_million,
+        new_deaths_smoothed_per_million
+    FROM {{ source('staging', 'covid-19') }}
 )
 
 SELECT * 
 FROM covid_19 AS c
 LEFT JOIN monkeypox AS m
     ON c.iso_code = m.monkeypox_iso_code AND c.date = m.monkeypox_date
-LIMIT 1000
+
