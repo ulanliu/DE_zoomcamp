78d9eba7ccdcd9a820a10be1f4f42d227b9c82e6
Peter Liu <f97290650@gmail.com>
2023-03-20 22:10:21 +0800

    Update w7 prefect flow

--- w7_final_project/prefect/el_web_to_gcs_flow.py
+++ w7_final_project/prefect/el_web_to_gcs_flow.py
@@ -12,10 +12,9 @@ import logging
 
 @task(log_prints=True)
 def extract(url: str) -> Path:
-    
-    # monkeypox_url = 'https://raw.githubusercontent.com/owid/monkeypox/main/owid-monkeypox-data.csv'
-    # covid_url = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv'
-
+    """
+    Download monkeypox and covid-19 daily data from Our World In Data into local
+    """
     day = date.today().strftime('%Y%m%d')
     if 'monkeypox' in url:
         path = Path(f'data/monkeypox_{day}.csv')
@@ -39,20 +38,19 @@ def extract(url: str) -> Path:
 
 @task(log_prints=True)
 def load_data_to_gcs(path: Path) -> None:
+    """
+    Load the data into GCS
+    """
     gcs_block = GcsBucket.load("zoomcamp-2")
     gcs_block.upload_from_path(from_path=path, to_path=path)
     gcs_path = path
     return gcs_path
 
 @flow()
-def el_web_to_gcs(url: str) -> None:
-    path = extract(url)
-    gcs_path = load_data_to_gcs(path)
-    load_data_to_bq(gcs_path)
-
-@flow
 def load_data_to_bq(gcs_path: Path):
-
+    """
+    Transfer data from GCS to BQ
+    """
     gcp_credentials_block = GcpCredentials.load("zoomcamp-2")
     
     if 'monkeypox' in str(gcs_path):
@@ -68,25 +66,15 @@ def load_data_to_bq(gcs_path: Path):
 
     return result
 
-
-# @flow
-# def load_data_to_bq():
-
-#     gcp_credentials_block = GcpCredentials.load("zoomcamp-2")
-#     day = date.today().strftime('%Y%m%d')
-#     for disease in ['monkeypox', 'covid-19']:
-    
-#         result = bigquery_load_file(
-#             dataset='who_disease_data',
-#             table=disease,
-#             path=Path(f"data/{disease}_{day}.csv"),
-#             gcp_credentials=gcp_credentials_block
-#         )
-
-#     return result
+@flow()
+def el_web_to_gcs(url: str) -> None:
+    path = extract(url)
+    gcs_path = load_data_to_gcs(path)
+    load_data_to_bq(gcs_path)
 
 @flow()
-def el_parent_flow(url_list: list[str]) -> None:
+def el_parent_flow(url_list: list[str]=["https://raw.githubusercontent.com/owid/monkeypox/main/owid-monkeypox-data.csv", "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv"]) -> None:
+    """ The main flow function """
     for url in url_list:
         el_web_to_gcs(url)
 
